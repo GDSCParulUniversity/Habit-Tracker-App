cc9e29a659429bafc3ca2b7503a5c90ce4e3164e
devbathani <devbathani@credicxo.com>
2022-10-09 13:52:45 +0530

    Textfield Added to the track screen and added screen utils

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:habit_tracker_app/screens/screens.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:habit_tracker_app/screens/track_screen.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,13 +11,21 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Habit Tracker App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SplashScreen(),
+    return ScreenUtilInit(
+      designSize: const Size(380, 720),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return MaterialApp(
+          debugShowCheckedModeBanner: false,
+          title: 'Habit Tracker App',
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: child,
+        );
+      },
+      child: TrackScreen(),
     );
   }
 }
--- lib/screens/track_screen.dart
+++ lib/screens/track_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/src/foundation/key.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:habit_tracker_app/common/constants/colors.dart';
+import 'package:habit_tracker_app/common/constants/headings.dart';
+import 'package:habit_tracker_app/common/constants/textstyles.dart';
 
 class TrackScreen extends StatelessWidget {
   const TrackScreen({Key? key}) : super(key: key);
@@ -8,8 +10,76 @@ class TrackScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(
-        
+      body: Padding(
+        padding: EdgeInsets.symmetric(horizontal: 10.w, vertical: 10.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              height: 30.h,
+            ),
+            Row(
+              children: [
+                IconButton(
+                  onPressed: () {
+                    Navigator.pop(context);
+                  },
+                  icon: Icon(
+                    Icons.arrow_back,
+                    size: 20.w,
+                    color: Colors.black,
+                  ),
+                ),
+                SizedBox(
+                  width: 10.w,
+                ),
+                Text(
+                  trackerAppBarHeading,
+                  style: headingStyle,
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Text(
+              trackerTitle,
+              style: subHeadingStyle,
+            ),
+            SizedBox(
+              height: 5.h,
+            ),
+            TextField(
+              cursorColor: redColor,
+              style:
+                  subHeadingStyle.copyWith(color: Colors.grey, fontSize: 15.sp),
+              decoration: InputDecoration(
+                contentPadding: EdgeInsets.symmetric(horizontal: 10.w),
+                focusColor: redColor.withOpacity(0.33),
+                fillColor: Colors.grey.shade100,
+                filled: true,
+                border: OutlineInputBorder(
+                  borderSide: BorderSide(color: redColor, width: 1),
+                  borderRadius: BorderRadius.circular(8.r),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.transparent, width: 1),
+                  borderRadius: BorderRadius.circular(8.r),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: redColor, width: 1),
+                  borderRadius: BorderRadius.circular(8.r),
+                ),
+                hintText: 'Task Title',
+                hintStyle: subHeadingStyle.copyWith(
+                    color: Colors.grey.shade300, fontSize: 15.sp),
+              ),
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+          ],
+        ),
       ),
     );
   }
