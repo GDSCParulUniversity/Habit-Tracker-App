14493efff643ac48f9715061784e14823c838de6
devbathani <devbathani@credicxo.com>
2022-10-09 13:09:15 +0530

    Track Screen setup initialized

--- lib/common/constants/colors.dart
+++ lib/common/constants/colors.dart
@@ -1,1 +1,3 @@
+import 'package:flutter/material.dart';
 
+const Color redColor = Colors.red;
---
+++ lib/common/constants/textstyles.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+final TextStyle headingStyle = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    color: Colors.black,
+    fontSize: 25,
+    fontWeight: FontWeight.bold,
+  ),
+);
+final TextStyle subHeadingStyle = GoogleFonts.poppins(
+  textStyle: TextStyle(
+    color: Colors.black,
+    fontSize: 20,
+    fontWeight: FontWeight.w500,  
+  ),
+);
